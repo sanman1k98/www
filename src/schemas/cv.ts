dd583abd5966d6fc216d2e7f7ec9d36877377f76
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-06-12 23:50:48 -0400

    build(content): add schemas for CV entries

---
+++ src/schemas/cv.ts
@@ -0,0 +1,84 @@
+import { z } from "astro:content";
+import { daterange, tags, org } from "./misc";
+
+export const base = z
+  .object({
+    type: z
+      .literal("base")
+      .optional()
+      .default("base"),
+    draft: z
+      .boolean()
+      .optional()
+      .default(false),
+  })
+  .strict();
+
+export const experience = base
+  .extend({
+    type: z.literal("experience"),
+    title: z
+      .string()
+      .describe("position title or role"),
+    employer: org,
+    internship: z
+      .boolean()
+      .optional()
+      .default(false)
+      .describe("true if this entry is an internship"),
+  })
+  .merge(daterange)
+  .merge(tags.partial());
+
+export const project = base
+  .extend({
+    type: z.literal("project"),
+    name: z.string(),
+    description: z
+      .string()
+      .max(65)
+      .describe("short description of the project"),
+    githubUrl: z
+      .string()
+      .url()
+      .describe("Link to GitHub repository"),
+  })
+  .merge(daterange)
+  .merge(tags);
+
+export const education = base
+  .extend({
+    type: z.literal("education"),
+    school: org,
+    major: z.string(),
+    degree: z.string(),
+  })
+  .merge(daterange);
+
+export const certification = base
+  .extend({
+    type: z.literal("certification"),
+    name: z
+      .string()
+      .describe("Name of the certification"),
+    organization: org
+      .omit({ location: true })
+      .describe("Organization which gave out the certification."),
+    issueDate: z
+      .date()
+      .describe("Date the certification was issued"),
+    expiryDate: z
+      .date()
+      .optional()
+      .describe("Date the certification expires"),
+  });
+
+export const volunteering = base
+  .extend({
+    type: z.literal("volunteering"),
+    title: z
+      .string()
+      .describe("Position title or role"),
+    organization: org,
+  })
+  .merge(daterange);
--- src/schemas/misc.ts
+++ src/schemas/misc.ts
@@ -9,3 +9,24 @@ export const link = z
         message: "href must begin with '/'",
       }),
   });
+
+export const daterange = z
+  .object({
+    start: z.coerce.date(),
+    end: z.coerce.date().optional(),
+  });
+
+export const tags = z
+  .object({
+    tags: z.string().array(),
+  });
+
+export const org = z
+  .object({
+    name: z.string(),
+    url: z
+      .string()
+      .url()
+      .optional(),
+    location: z.string(),
+  });
