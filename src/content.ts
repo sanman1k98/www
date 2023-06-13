d00fb2243ad4af3e5e1f8ff25ab9c4b631357575
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-06-13 00:16:34 -0400

    refactor: add helpers for the "cv" collection

--- src/content.ts
+++ src/content.ts
@@ -1,26 +1,40 @@
-import { getCollection } from "astro:content";
-import type { CollectionEntry, z } from "astro:content";
+import {
+  getCollection,
+  type CollectionEntry,
+  type z,
+} from "astro:content";
+
 import type {
+  cvSchema,
   infoSchema,
   collections,
 } from "@/content/config";
 
 export type CollectionKey = keyof typeof collections;
 
+type CvEntryData = z.infer<typeof cvSchema>;
 type InfoEntryData = z.infer<typeof infoSchema>;
 
+export type CvEntryType = CvEntryData["type"];
 export type InfoEntryType = InfoEntryData["type"];
+
+export type CvEntry<T extends CvEntryType> = CollectionEntry<"cv"> & {
+  data: Extract<CvEntryData, { type: T }>;
+};
 
 export type InfoEntry<T extends InfoEntryType> = CollectionEntry<"info"> & {
   data: Extract<InfoEntryData, { type: T }>;
 };
 
+export function createCvEntryTypeGuard<T extends CvEntryType>(type: T) {
+  return (entry: CollectionEntry<"cv">): entry is CvEntry<T> => entry.data.type === type;
+}
+
 export function createInfoEntryTypeGuard<T extends InfoEntryType>(type: T) {
   return (entry: CollectionEntry<"info">): entry is InfoEntry<T> => entry.data.type === type;
 }
 
+export const cvEntries = await getCollection("cv");
 export const infoEntries = await getCollection("info");
 
 export const siteInfo = infoEntries.find(entry => entry.id === "site") as InfoEntry<"links">;
-
-export const linksEntries = infoEntries.filter(createInfoEntryTypeGuard("links"))
