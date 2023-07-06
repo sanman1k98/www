2ad372c9bfea178e22b5a679aad6decf64823844
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-07-06 16:16:48 -0400

    refactor: export sorted `cvEntries`
    Create a new helper function "compareCvEntryDateranges" to export
    `cvEntries` in reverse-chronological order.

--- src/content.ts
+++ src/content.ts
@@ -10,6 +10,8 @@ import type {
   collections,
 } from "@/content/config";
 
+import type { daterange } from "@/schemas/misc";
+
 export type CollectionKey = keyof typeof collections;
 
 type CvEntryData = z.infer<typeof cvSchema>;
@@ -34,7 +36,23 @@ export function createInfoEntryTypeGuard<T extends InfoEntryType>(type: T) {
   return (entry: CollectionEntry<"info">): entry is InfoEntry<T> => entry.data.type === type;
 }
 
-export const cvEntries = await getCollection("cv");
+export function compareCvEntryDateranges<
+  E extends CollectionEntry<"cv"> & {
+    data: Partial<z.infer<typeof daterange>>;
+  }
+>(a: E, b: E) {
+  if (!(a.data.start && b.data.start)) return 0;
+  const present = Date.now().valueOf();
+  const [startA, startB, endA, endB] = [
+    a.data.start.valueOf(),
+    b.data.start.valueOf(),
+    a.data.end?.valueOf() ?? present,
+    b.data.end?.valueOf() ?? present,
+  ];
+  return endA - endB || startA - startB;
+}
+
+export const cvEntries = (await getCollection("cv")).sort(compareCvEntryDateranges).reverse();
 export const infoEntries = await getCollection("info");
 
 export const contact = infoEntries.find(entry => entry.id === "secret/contact") as InfoEntry<"contact">;
