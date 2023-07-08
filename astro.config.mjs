c7f747c4fc5b23c37c5375524772f368630f9d6e
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-07-08 18:21:24 -0400

    refactor: use `UnoCSS`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import UnoCSS from "unocss/astro";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch()]
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    prefetch(),
+  ],
 });
