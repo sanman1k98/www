9a7b8473caafe77ed2ee63b1b3fab923d6460d1f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-07-13 17:21:27 -0400

    fix: parsed env vars

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,4 +10,8 @@ declare global {
   }
 };
 
-export const env = envSchema.parse(import.meta.env);
+// TODO: use a library like `@t3-oss/env-core` for env vars validation
+
+// FIXME: This is a workaround for Vite statically replacing `import.meta.env`
+// with the actual values.
+export const env = envSchema.parse(process.env);
