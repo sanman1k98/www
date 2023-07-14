9653723da96ab3cf2749d0e65345f516cdd6d341
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-07-14 16:48:41 -0400

    feat: add Cascadia Code web font

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig, presetUno } from "unocss"
+import { presetWebFonts } from "unocss";
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetWebFonts({
+      provider: "none",
+      fonts: {
+        mono: ["Cascadia Code PL",],
+      },
+    }),
   ],
 });
