32a6000ef5997911dde0e7b625a1eed2307ea1da
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-07-19 16:05:54 -0400

    refactor(env)!: change env vars schema
    Prefix "PHONE" and "EMAIL" with "PERSONAL_".
    
    Add an optional environment "BUILD_INCLUDE_PERSONAL" to be validated in
    the schema.
    
    BREAKING CHANGE: Phone and email variables are no longer optional.

--- src/schemas/env.ts
+++ src/schemas/env.ts
@@ -1,6 +1,36 @@
 import { z } from "astro/zod";
 
 export default z.object({
-  PHONE: z.string(),
-  EMAIL: z.string().email(),
-}).partial();
+  /**
+   * To be displayed on the resume page when printing.
+   */
+  PERSONAL_PHONE: z.string(),
+
+  /**
+   * To be displayed on the resume page when printing.
+   */
+  PERSONAL_EMAIL: z.string().email(),
+
+  /**
+   * Use to display personal contact information on the resume page.
+   * 
+   * When set to "true" and running `astro dev` or `astro build`, Astro will
+   * generate static HTML files containing personal information.
+   * 
+   * @example Create a Resume PDF with contact information
+   * ```sh
+   * BUILD_INCLUDE_PERSONAL=true bun run astro build
+   * ```
+   * You can then go to http://localhost:3000/resume and print the page and save
+   * as a PDF which will contain personal contact information.
+   *
+   * @remarks
+   * **WARNING**: Setting `display: none` in CSS will **not** remove data from
+   * the HTML. **DO NOT** upload the generated files to a public repository if
+   * set to "true" because they will contain sensitive information.
+   */
+  BUILD_INCLUDE_PERSONAL: z
+    .enum(["true", "false"])
+    .default("false")
+    .transform(str => str === "true"),
+});
