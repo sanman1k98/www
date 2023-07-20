3aab44fdded0219d1aa4a7c248075f4c796ec403
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-07-20 10:50:41 -0400

    refactor: remove comment
    I think parsing `process.env` directly is a good enough solution and I
    don't think we will run into any problems in the foreseeable future
    using it.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,8 +10,4 @@ declare global {
   }
 };
 
-// TODO: use a library like `@t3-oss/env-core` for env vars validation
-
-// FIXME: This is a workaround for Vite statically replacing `import.meta.env`
-// with the actual values.
 export const env = envSchema.parse(process.env);
