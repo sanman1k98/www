09bd67f08ba27c4e593ed184dbeec3fa1fc565dc
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-07-22 16:59:45 -0400

    feat(content): optional "title" for links entry

--- src/schemas/info.ts
+++ src/schemas/info.ts
@@ -3,16 +3,17 @@ import { link } from "./misc";
 
 export const base = z
   .object({
-    type: z
-      .literal("base")
-      .default("base"),
+    type: z.literal("base").default("base"),
   })
   .strict();
 
 export const links = base
   .extend({
-    type: z
-      .literal("links")
-      .default("links"),
+    type: z.literal("links").default("links"),
+
+    /** Describes the links in this entry. */
+    title: z.string().default("Links"),
+
+    /** The list of links. */
     links: link.array(),
   });
