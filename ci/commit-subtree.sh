293223f992b9b10689b051df855d640c3d89b2af
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-08-26 19:12:26 -0400

    ci: sign commit

--- ci/commit-subtree.sh
+++ ci/commit-subtree.sh
@@ -11,9 +11,9 @@ git add --force "${SUBTREE}"
 tree_hash="$(git write-tree --prefix="${SUBTREE}/")"
 
 # create commit and get its id
-commit_id="$(git commit-tree -p "${BRANCH}" -m 'ci: deploy' "${tree_hash}")"
+commit_id="$(git commit-tree --gpg-sign -p "${BRANCH}" -m 'ci: deploy' "${tree_hash}")"
 
 # update branch to point to new commit
 git update-ref "refs/heads/${BRANCH}" "${commit_id}"
 
-git show "${commit_id}"
+git show --show-signature "${commit_id}"
