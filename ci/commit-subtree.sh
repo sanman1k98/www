01ea397a2f469215edd7aea78bd2d9ac2b30bad6
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-08-26 17:45:43 -0400

    ci: script to commit "dist/"

---
+++ ci/commit-subtree.sh
@@ -0,0 +1,14 @@
+BRANCH="deploy"
+SUBTREE="dist"
+
+# add static files to the index
+git add --force "${SUBTREE}"
+
+# create a new tree object and get its hash
+tree_hash="$(git write-tree --prefix="${SUBTREE}/")"
+
+# create commit and get its id
+commit_id="$(git commit-tree --parent "${BRANCH}" --message 'ci: deploy' "${tree_hash}")"
+
+# update branch to point to new commit
+git update-ref "refs/heads/${BRANCH}" "${commit_id}"
