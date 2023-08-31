eddd0d150e52e18e95cd2bbe401534363e09f9c2
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-08-31 17:15:18 -0400

    build(css): configure "Carbon" icons

--- uno.config.ts
+++ uno.config.ts
@@ -2,11 +2,17 @@ import {
   defineConfig,
   presetUno,
   presetWebFonts,
+  presetIcons,
 } from "unocss"
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetIcons({
+      collections: {
+        carbon: () => import("@iconify-json/carbon/icons.json").then(i => i.default),
+      }
+    }),
     presetWebFonts({
       provider: "none",
       fonts: {
