4e42d9759290553035302c820f9c9ea41674be8e
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-09-10 18:20:37 -0400

    build(css): add Panda CSS
    I've heard good things but I don't really know much about it so we're
    gonna take it out for a test drive.

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from "unocss/astro";
+import pandacss from "@pandacss/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nicesandeep.com",
   integrations: [
+    pandacss(),
     UnoCSS({
       // When passing true, "@unocss/reset/tailwind.css" will be used
       injectReset: true,
