0076a52864492820acd733e5d4bdbbca9429fb60
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-02-16 11:27:33 -0500

    refactor(content): rename key in "photos" schema

--- src/schemas/photos.ts
+++ src/schemas/photos.ts
@@ -6,21 +6,23 @@ const PHOTOS_COLLECTION_DIR = new URL("../content/photos/", import.meta.url);
 
 export const photos = ({ image }: SchemaContext) => z
   .object({
-    src: image(),
+    /** Relative path to an image file. */
+    file: image(),
+    /** An empty string tells screen readers to ignore the image. */
     alt: z.string().default(""),
   })
   .transform(
-    async (val) => {
-      const fileURL = new URL(val.src.src, "file://");
+    async (photo) => {
+      const fileURL = new URL(photo.file.src, "file://");
       const imagePath = fileURL.pathname.slice(4);
       // console.log(fileURL);
       // const { exif } = await sharp(imagePath).metadata();
-      return { ...val };
+      return { ...photo };
     }
   )
   .array()
   .transform(
     arr => arr.map(
-      (val, i) => ({ slug: i + 1, ...val })
+      (photo, i) => ({ slug: i + 1, ...photo })
     )
   );
