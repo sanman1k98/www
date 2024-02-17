55c9032b272aaa467c143d29eabddb48907ce90c
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-02-17 15:44:54 -0500

    refactor(content): add comments and small changes

--- src/schemas/photos.ts
+++ src/schemas/photos.ts
@@ -1,24 +1,24 @@
 import { z, type SchemaContext } from "astro:content";
 import { fileURLToPath } from "node:url";
 import sharp from "sharp";
-import exifReader from "exif-reader";
-import type { Exif, GenericTag } from "exif-reader";
+import exifReader, { type Exif } from "exif-reader";
 
 const PHOTOS_COLLECTION_DIR = new URL("../content/photos/", import.meta.url);
 
+// I think these "groups" are actually called *Image File Directories*
+// see: https://stackoverflow.com/questions/1821515/how-is-exif-info-encoded#14115795
 type TagGroup = keyof Omit<Exif, "bigEndian">;
-type ExifTags = {
-  [key in TagGroup]?: Exif[key];
-};
+type ExifTags = { [key in TagGroup]?: Exif[key] };
 
-const stringifyExifBuffers = (data: ExifTags) => {
+/** Converts Buffers into number arrays. */
+const fixExif = (data: ExifTags) => {
   for (const group of Object.entries(data)) {
     const [_, tags] = group as [TagGroup, NonNullable<ExifTags[TagGroup]>];
     for (const tag of Object.entries(tags)) {
       const [name, value] = tag;
       // @ts-ignore
       tags[name] = value instanceof Buffer
-        ? JSON.stringify(value)
+        ? Array.from(value)
         : value;
     }
   }
@@ -30,10 +30,12 @@ export const photos = ({ image }: SchemaContext): z.ZodSchema => {
     .object({
       /** Relative path to image file. */
       file: z.string(),
+      /** Alt text for screenreaders. */
       alt: z.string().default("")
     })
     .transform(
       async ({ file: path, ...rest }, ctx) => {
+        // Use Astro's image helper first
         const transformed = await image().safeParseAsync(path);
         if (!transformed.success) {
           ctx.addIssue({
@@ -43,15 +45,21 @@ export const photos = ({ image }: SchemaContext): z.ZodSchema => {
           });
           return z.NEVER;
         }
+
+        // Get the absolute path to the original image
         const fileURL = new URL(path, PHOTOS_COLLECTION_DIR);
-        const resolvedPath = fileURLToPath(fileURL);
-        const { exif: buf } = await sharp(resolvedPath).metadata();
-        const parsedExif = buf ? exifReader(buf) : null;
-        if (!parsedExif) return { file: transformed.data, ...rest }
-        const { bigEndian: _, ...exifTags } = parsedExif;
+        const imagePath = fileURLToPath(fileURL);
+
+        // Give sharp path to image
+        const { exif: buf } = await sharp(imagePath).metadata();
+        // Return without metadata if no EXIF present
+        if (!buf) return { file: transformed.data, ...rest };
+
+        const { bigEndian: _, ...exifTags } = exifReader(buf);
         return {
           file: transformed.data,
-          exif: stringifyExifBuffers(exifTags),
+          // Astro only allows POJOs in Data entries
+          exif: fixExif(exifTags),
           ...rest
         };
       }
