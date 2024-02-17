69ec7712ed3bcea955b8522b2e0617471cbaf5e8
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-02-17 14:50:26 -0500

    impr(content): parse all EXIF metadata

--- src/schemas/photos.ts
+++ src/schemas/photos.ts
@@ -1,43 +1,58 @@
-import { z } from "astro/zod";
-import type { SchemaContext } from "astro:content";
-import sharp from "sharp";
-import exif from "exif-reader";
+import { z, type SchemaContext } from "astro:content";
 import { fileURLToPath } from "node:url";
+import sharp from "sharp";
+import exifReader from "exif-reader";
+import type { Exif, GenericTag } from "exif-reader";
 
 const PHOTOS_COLLECTION_DIR = new URL("../content/photos/", import.meta.url);
 
-export const photos = ({ image }: SchemaContext): z.ZodSchema => {
-  /** Custom image helper to include original path to image file. */
-  const customHelper = z
-    .string()
-    .transform(str => ({ path: str, emittedAsset: str }))
-    .pipe(z.object({ path: z.string(), emittedAsset: image() }))
-    .transform(obj => ({ path: obj.path, ...obj.emittedAsset }));
+type TagGroup = keyof Omit<Exif, "bigEndian">;
+type ExifTags = {
+  [key in TagGroup]?: Exif[key];
+};
 
+const stringifyExifBuffers = (data: ExifTags) => {
+  for (const group of Object.entries(data)) {
+    const [_, tags] = group as [TagGroup, NonNullable<ExifTags[TagGroup]>];
+    for (const tag of Object.entries(tags)) {
+      const [name, value] = tag;
+      // @ts-ignore
+      tags[name] = value instanceof Buffer
+        ? JSON.stringify(value)
+        : value;
+    }
+  }
+  return data;
+}
+
+export const photos = ({ image }: SchemaContext): z.ZodSchema => {
   return z
     .object({
       /** Relative path to image file. */
-      file: customHelper,
+      file: z.string(),
       alt: z.string().default("")
     })
     .transform(
-      async (photo) => {
-        const fileURL = new URL(photo.file.path, PHOTOS_COLLECTION_DIR);
+      async ({ file: path, ...rest }, ctx) => {
+        const transformed = await image().safeParseAsync(path);
+        if (!transformed.success) {
+          ctx.addIssue({
+            code: "custom",
+            message: `Image ${path} does not exist.`,
+            fatal: true
+          });
+          return z.NEVER;
+        }
+        const fileURL = new URL(path, PHOTOS_COLLECTION_DIR);
         const resolvedPath = fileURLToPath(fileURL);
-        const { exif: exifBuffer } = await sharp(resolvedPath).metadata();
-        const metadata = exifBuffer ? exif(exifBuffer) : null;
+        const { exif: buf } = await sharp(resolvedPath).metadata();
+        const parsedExif = buf ? exifReader(buf) : null;
+        if (!parsedExif) return { file: transformed.data, ...rest }
+        const { bigEndian: _, ...exifTags } = parsedExif;
         return {
-          metadata: {
-            camera: metadata?.Image?.Model,
-            lens: metadata?.Photo?.LensModel,
-            iso: metadata?.Photo?.ISOSpeedRatings,
-            shutter: metadata?.Photo?.ExposureTime,
-            aperture: metadata?.Photo?.ApertureValue,
-            focalLength: metadata?.Photo?.FocalLength,
-            focalLengthIn35mmFilm: metadata?.Photo?.FocalLengthIn35mmFilm,
-            time: metadata?.Photo?.DateTimeOriginal,
-          },
-          ...photo
+          file: transformed.data,
+          exif: stringifyExifBuffers(exifTags),
+          ...rest
         };
       }
     )
