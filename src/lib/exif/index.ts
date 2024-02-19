1d2ebcd8be44b328ef0250aee8503bdc4c1eff34
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-02-19 15:16:53 -0500

    feat(photos): show EXIF metadata for each photo

---
+++ src/lib/exif/index.ts
@@ -0,0 +1,20 @@
+import type { Exif } from "exif-reader";
+
+/** If the exposure time is < 1, it will return a fraction. */
+export function getShutterSpeed(exposureTime: number): string {
+  if (exposureTime >= 1) return exposureTime.toString();
+  const reciprocal = 1 / exposureTime;
+  return `1/${reciprocal}`;
+}
+
+export function getExposureComp(value: number): string {
+  if (!Number.isInteger(value)) return value.toFixed(1);
+  return value.toString();
+}
+
+export function getLensInfo(exif: Exif): string | undefined {
+  if (exif.Photo?.LensMake === "Apple") {
+    return `${exif.Photo.FocalLengthIn35mmFilm} mm f/${exif.Photo.FNumber}`;
+  }
+  return exif.Photo?.LensModel;
+}
