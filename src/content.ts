50c1bc06b7caf81607dd2e588f210e9905579f10
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-24 12:51:22 -0400

    refactor(content): move sorting logic to "/resume"
    Instead of exporting a (mostly) sorted collection, allow the "/resume"
    page to do the sorting.
    
    Also export another function "compareOrderedEntries", so that the
    original "compareCvEntries" function isn't responsible for sorting both
    by the "order" values and dates.

--- src/content.ts
+++ src/content.ts
@@ -27,55 +27,39 @@ export function createInfoEntryTypeGuard<T extends InfoEntryType>(type: T) {
   return (entry: CollectionEntry<"info">): entry is InfoEntry<T> => entry.data.type === type;
 }
 
-type SortableCvEntry = CollectionEntry<"cv"> & { data: Partial<z.infer<typeof daterange>> };
-
-const present = new Date();
-let cvStart = present;
-let foundCvStart = false;
-
-/**
- * A callback to sort entries in the "cv" collection by comparing which one
- * happened most recently, or by their specified orders.
- *
- * Entries can be compared using values from the following fields if they exist
- * on both, and are listed here from highest to lowest priority:
- * 1. "order"
- * 2. "end"
- * 3. "start"
- */
-export function compareCvEntries(a: SortableCvEntry, b: SortableCvEntry): number {
-  // Check "order" values first
+export function compareOrderedEntries(a: CollectionEntry<"cv">, b: CollectionEntry<"cv">): number {
   const [orderA, orderB] = [a.data.order, b.data.order];
   if (orderA !== undefined && orderB !== undefined)
     return orderA - orderB;
+  else return 0;
+}
+
+type ChronologicalEntry = CollectionEntry<"cv"> & {
+  data: Extract<CvEntryData, z.infer<typeof daterange>>;
+};
+
+const PRESENT = new Date();
+/**
+ * A callback to sort entries in the "cv" collection by comparing which one
+ * happened most recently.
+ */
+export function compareChronologicalEntries(a: ChronologicalEntry, b: ChronologicalEntry): number {
   // If and entry doesn't have a "start" they def don't have an "end", so we'll
   // return 0 so they won't get sorted.
-  else if (!(a.data.start && b.data.start))
+  if (!(a.data.start && b.data.start))
     return 0;
   const [startA, startB, endA, endB] = [
     a.data.start.valueOf(),
     b.data.start.valueOf(),
-    a.data.end?.valueOf() ?? present.valueOf(),
-    b.data.end?.valueOf() ?? present.valueOf(),
+    a.data.end?.valueOf() ?? PRESENT.valueOf(),
+    b.data.end?.valueOf() ?? PRESENT.valueOf(),
   ];
-  if (!foundCvStart) {
-    if (startA < cvStart.valueOf())
-      cvStart = a.data.start;
-    else if (startB < cvStart.valueOf())
-      cvStart = b.data.start;
-  }
   // JS considers 0 to be falsy
   return endA - endB || startA - startB;
 }
 
-// An empty collection will return `undefined` i.e., when the "cv" git submodule is empty
-const maybeCvEntries: SortableCvEntry[] = await getCollection("cv") ?? [];
-
-/** Sorted in (mostly) reverse chronological order. */
-export const cvEntries: CollectionEntry<"cv">[] = maybeCvEntries.sort(compareCvEntries).reverse();
-
-// Set this to true because we just sorted `cvEntries` using `compareCvEntries`
-foundCvStart = true;
+export const cvEntries = await getCollection("cv")
+  .then(entries => entries.filter(({ data }) => !data.draft));
 
 /** The earliest date specified in an entry from the "cv" collection. */
 export const CV_START = cvEntries
