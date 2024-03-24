541307703478174ca622fde4d117978fe8c574c3
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-24 12:31:55 -0400

    refactor(content): extract `href` schema

--- src/schemas/misc.ts
+++ src/schemas/misc.ts
@@ -1,6 +1,19 @@
 import type { HTMLAttributes } from "astro/types";
 import { z } from "astro/zod";
 
+export const href = z.union([
+  z.string().url(),
+  z.string().refine((str) => {
+    switch (true) {
+      case str.startsWith("/"): return true;
+      case str.startsWith("#"): return true;
+      case str.startsWith("mailto:"): return true;
+      case str.startsWith("tel:"): return true;
+      default: return false;
+    }
+  }),
+]);
+
 type Link = HTMLAttributes<"a"> & {
   text: string;
   href: string;
@@ -8,36 +21,11 @@ type Link = HTMLAttributes<"a"> & {
 };
 
 /** Use to create HTMLAnchorElements. */
-export const link = z
-  .object({
-
-    /** **Required**: string to set the inner text of the anchor element. */
-    text: z.string(),
-
-    /** **Required**: URL to set the href attribute of the anchor element. */
-    href: z.union([
-      z.string().url(),
-      z.string().refine((s) => {
-        switch (true) {
-          case s.startsWith("/"): return true;
-          case s.startsWith("#"): return true;
-          case s.startsWith("mailto:"): return true;
-          case s.startsWith("tel:"): return true;
-          default: return false;
-        }
-      }),
-    ]),
-
-    /** **Optional**: where to load the URL; defaults to "_self". */
-    target: z
-      .enum([
-        "_blank",
-        "_self",
-        "_parent",
-        "_top",
-      ])
-      .optional(),
-
-    /** **Optional**: An Iconify icon name. */
-    icon: z.string().optional(),
-  }) satisfies z.ZodType<Link>;
+export const link = z.object({
+  /** String to set the inner text of the anchor element. */
+  text: z.string(),
+  href,
+  target: z.enum(["_blank", "_self", "_parent", "_top"]).optional(),
+  /** **Optional**: An Iconify icon name. */
+  icon: z.string().optional(),
+}) satisfies z.ZodType<Link>;
