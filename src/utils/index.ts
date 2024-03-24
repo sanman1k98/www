462b5cf5d34462235418a6e4a1f5322cf8d9eb69
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-24 14:42:14 -0400

    refactor(utils): export better typed `unoTheme`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,7 @@
-import type { Theme, colors } from "@unocss/preset-mini";
+import type { theme as miniTheme } from "@unocss/preset-mini";
 import { resolveConfig } from "@unocss/core";
 import unoConfig from "uno.config";
 
-
 export { env } from "./env";
 
-export const resolvedUnoConfig = resolveConfig<Theme>(unoConfig);
-
-export const unoBreakpoints = resolvedUnoConfig.theme.breakpoints ?? {};
-
-export const unoColors = resolvedUnoConfig.theme.colors as typeof colors;
+export const { theme: unoTheme } = resolveConfig<typeof miniTheme>(unoConfig);
