6dbc642b169029b0ed02efd01fdae67b1ec8bed2
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-24 20:48:28 -0400

    impr(photos): display lens info
    Slightly different layout for caption under photo.

--- src/utils/exif.ts
+++ src/utils/exif.ts
@@ -1,4 +1,32 @@
-import type { Exif } from "exif-reader";
+import type { Photo } from "@/content";
+
+type Exif = Photo["exif"];
+
+type FocalLengthPair = [
+  FocalLength: number,
+  FocalLengthin35mmFilm: number,
+];
+
+interface iPhoneFocalLengths {
+  UltraWide: FocalLengthPair;
+  Main: FocalLengthPair;
+  Telephoto: FocalLengthPair;
+}
+
+const IPHONE_FOCAL_LENGTHS = {
+  "iPhone 11 Pro": {
+    UltraWide: [1.54, 13],
+    Main: [4.25, 26],
+    Telephoto: [6, 52],
+  },
+  "iPhone 15 Pro": {
+    UltraWide: [2.22, 13],
+    Main: [6.765, 24],
+    Telephoto: [9, 77],
+  },
+} as const satisfies { [k: string]: iPhoneFocalLengths };
+
+type iPhoneModel = keyof typeof IPHONE_FOCAL_LENGTHS;
 
 /** If the exposure time is < 1, it will return a fraction. */
 export function getShutterSpeed(exposureTime: number | undefined): string {
@@ -18,9 +46,82 @@ export function getExposureComp(value: number | undefined): string {
   return value.toString();
 }
 
-export function getLensInfo(exif: Exif): string | undefined {
-  if (exif.Photo?.LensMake === "Apple")
-    return `${exif.Photo.FocalLengthIn35mmFilm} mm f/${exif.Photo.FNumber}`;
+function formatiPhoneCamera(camera: string, focalLength: number, fNumber: number) {
+  return `${camera} Camera - ${focalLength} mm f/${fNumber}`;
+}
 
-  return exif.Photo?.LensModel;
+function getiPhoneCamera(model: iPhoneModel, tags: Exif["Photo"]): string | undefined {
+  const focalLengthIn35mmFilm = tags.FocalLengthIn35mmFilm;
+  const fNumber = tags.FNumber;
+
+  if (!(focalLengthIn35mmFilm && fNumber))
+    return undefined;
+
+  if (tags.LensModel?.includes("front"))
+    return formatiPhoneCamera("Front", focalLengthIn35mmFilm, fNumber);
+
+  const focalLengths = IPHONE_FOCAL_LENGTHS[model];
+  let camera: string;
+
+  switch (tags.FocalLength) {
+    case focalLengths.UltraWide[0]:
+      camera = "Ultra Wide";
+      break;
+    case focalLengths.Main[0]:
+      camera = "Main";
+      break;
+    case focalLengths.Telephoto[0]:
+      camera = "Telephoto";
+      break;
+    default:
+      return undefined;
+  }
+
+  return formatiPhoneCamera(camera, focalLengthIn35mmFilm, fNumber);
+}
+
+export function getLensInfo(exif: Exif): string | undefined {
+  if (exif.Photo.LensMake !== "Apple")
+    return exif.Photo.LensModel;
+
+  const iphoneModel = exif.Image.Model as iPhoneModel;
+
+  if (!(iphoneModel in IPHONE_FOCAL_LENGTHS))
+    return undefined;
+  else
+    return getiPhoneCamera(iphoneModel, exif.Photo);
+}
+
+export function getCamera(exif: Exif): string | undefined {
+  return exif.Image?.Model;
+}
+
+export function getReadableCaptureSettings(tags: Exif["Photo"]): string[] {
+  const iso = tags.ISOSpeedRatings?.toString() ?? "---";
+  const focallen = (tags.FocalLengthIn35mmFilm ?? tags.FocalLength)?.toString() ?? "--";
+  const ev = getExposureComp(tags.ExposureBiasValue);
+  const f = tags.FNumber?.toString() ?? "-.-";
+  const shutter = getShutterSpeed(tags.ExposureTime);
+
+  /** Inspired by the metadata viewer in the iOS Photos app. */
+  return [
+    `ISO ${iso}`,
+    `${focallen} mm`,
+    `${ev} ev`,
+    `f/${f}`,
+    `${shutter}s`,
+  ];
+}
+
+export function getCaptureTimeStrings(date: Date | undefined) {
+  if (date === undefined)
+    return null;
+  return {
+    ISOString: date.toISOString(),
+    formatted: date.toLocaleString("en", {
+      dateStyle: "medium",
+      timeStyle: "short",
+      timeZone: "UTC",
+    }),
+  };
 }
