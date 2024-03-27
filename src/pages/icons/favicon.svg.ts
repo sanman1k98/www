25de19eacfa77c3b0d265cba5be83963908f44b9
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-27 10:47:20 -0400

    refactor: use dynamic route for `favicon.svg`

--- src/pages/icons/favicon.svg.ts
+++
@@ -1,4 +0,0 @@
-import type { APIRoute } from "astro";
-import { generateIcon } from "@/components/Favicon";
-
-export const GET: APIRoute = async () => new Response(await generateIcon(), { headers: [["Content-Type", "image/svg+xml"]] });
