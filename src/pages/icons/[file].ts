27cfc8f3c16b9479d321fe7fee270456beec748f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-27 10:43:07 -0400

    chore: add dynamic endpoint `/icons/[files].ts`

---
+++ src/pages/icons/[file].ts
@@ -0,0 +1,32 @@
+import type { APIRoute, GetStaticPaths, InferGetStaticParamsType } from "astro";
+import { generateIcon } from "@/components/Favicon";
+
+export const getStaticPaths = (() => {
+  const FILES = [
+    "favicon.svg",
+  ] as const;
+
+  return FILES.map(file => ({ params: { file } }));
+}) satisfies GetStaticPaths;
+
+type Params = InferGetStaticParamsType<typeof getStaticPaths>;
+
+export const GET: APIRoute<any, Params> = async ({ params }) => {
+  const { file } = params;
+
+  let body: BodyInit;
+  let contentType: string;
+
+  switch (file) {
+    case "favicon.svg":
+      body = await generateIcon();
+      contentType = "image/svg+xml";
+      break;
+  }
+
+  return new Response(body, {
+    headers: [
+      ["Content-Type", contentType],
+    ],
+  });
+};
