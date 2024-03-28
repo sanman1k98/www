aa4a28835a99e36238443f5c5a08d24c68dd456a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-27 21:44:23 -0400

    feat(seo): PNG favicon in endpoint #36

--- src/pages/icons/[file].ts
+++ src/pages/icons/[file].ts
@@ -1,9 +1,12 @@
+import { Buffer } from "node:buffer";
+import sharp from "sharp";
 import type { APIRoute, GetStaticPaths, InferGetStaticParamsType } from "astro";
 import * as Icon from "./_Icon";
 
 export const getStaticPaths = (() => {
   const FILES = [
     "favicon.svg",
+    "favicon.png",
   ] as const;
 
   return FILES.map(file => ({ params: { file } }));
@@ -14,13 +17,21 @@ type Params = InferGetStaticParamsType<typeof getStaticPaths>;
 export const GET: APIRoute<any, Params> = async ({ params }) => {
   const { file } = params;
 
+  const promise = Icon.renderToSVG(Icon.NSGradient);
   let body: BodyInit;
   let contentType: string;
 
   switch (file) {
     case "favicon.svg":
-      body = await Icon.renderToSVG(Icon.NSGradient);
+      body = await promise.then(str => str);
       contentType = "image/svg+xml";
+      break;
+    case "favicon.png":
+      body = await promise.then((str) => {
+        const buf = Buffer.from(str);
+        return sharp(buf).toBuffer();
+      });
+      contentType = "image/png";
       break;
   }
 
