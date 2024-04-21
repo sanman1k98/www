b7dec5f3e39a965847856941fa8c363fa7810313
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-04-20 22:17:06 -0400

    refactor: rewrite cache implementation again

--- src/utils/cache.test.ts
+++ src/utils/cache.test.ts
@@ -1,25 +1,177 @@
-import fs from "node:fs";
-import { describe, expect, it } from "bun:test";
+/* eslint-disable no-console */
+/* eslint-disable dot-notation */
+import fs, { type PathLike } from "node:fs";
+import { serialize } from "node:v8";
+import { afterAll, afterEach, beforeAll, describe, expect, it, jest, spyOn } from "bun:test";
 import { CACHES_DIR, cache } from "./cache";
 
-describe("cache", async () => {
-  const REQ_URL = "https://api.github.com/repos/sanman1k98/www";
-  const CACHED_RES_SUBDIR = "./v0/api.github.com/repos-sanman1k98-www/";
+describe("Bun runtime", async () => {
+  it("cannot serialize a Response", async () => {
+    const res = new Response("Hello World!");
+    let err;
 
-  it("creates a directory containing cache files", () => {
+    try {
+      serialize(res);
+    }
+    catch (e) {
+      err = e;
+    }
+
+    expect(err).toHaveProperty("message", "The object can not be cloned.");
+  });
+});
+
+describe("cache", async () => {
+  it("created a directory containing cache files", () => {
     expect(fs.existsSync(CACHES_DIR)).toBeTrue();
   });
 
-  it.skip("can save request/response pairs", async () => {
-    const res = await fetch(REQ_URL);
-    const promise = cache.put(REQ_URL, res);
-    expect(promise).resolves.toBeUndefined();
-    const dir = new URL(CACHED_RES_SUBDIR, CACHES_DIR);
-    expect(fs.existsSync(dir));
+  it("can serialize and deserialize a Response object", async () => {
+    const body = { message: "Hello world!", foo: "bar" };
+    const date = new Date().toUTCString();
+    const res = Response.json(
+      body,
+      {
+        status: 200,
+        statusText: "OK",
+        headers: { date },
+      },
+    );
+
+    const serialized = await cache["serializeResponse"](res.clone());
+    const alsoRes = await cache["deserializeResponse"](serialized);
+
+    expect(alsoRes.json()).resolves.toEqual(body);
+    expect(alsoRes.status).toBe(res.status);
+    expect(alsoRes.statusText).toBe(res.statusText);
+    expect(alsoRes.headers.toJSON()).toEqual(res.headers.toJSON());
   });
 
-  it.todo("can return a response for the associated request", () => {
-    const promise = cache.match(REQ_URL);
-    expect(promise).resolves.toBeInstanceOf(Response);
+  it("can hash RequestInfo and get a path", async () => {
+    const req = new Request("https://www.example.com");
+    const url = new URL("https://www.example.com");
+    const str = "https://www.example.com";
+
+    const reqHash = await cache["hashRequestInfo"](req);
+    const urlHash = await cache["hashRequestInfo"](url);
+    const strHash = await cache["hashRequestInfo"](str);
+
+    expect(reqHash).toBeString();
+    expect(urlHash).toBeString();
+    expect(strHash).toBeString();
+
+    expect(reqHash).toBe(urlHash);
+    expect(urlHash).toBe(strHash);
+
+    const pathlike = cache["path"](reqHash);
+
+    if (pathlike instanceof URL) {
+      expect(pathlike.protocol).toBe("file:");
+      expect(pathlike.pathname).toInclude(cache["dirname"].pathname);
+    }
+  });
+
+  describe("put()", async () => {
+    const req = new Request("https://www.example.com");
+    const res = Response.json({ foo: "bar" });
+    const path = cache["path"](await cache["hashRequestInfo"](req));
+
+    it("can save a Response as a file", async () => {
+      const putPromise = cache.put(req, res);
+      expect(putPromise).resolves.toBe(undefined);
+
+      await putPromise;
+
+      const fd = await fs.promises.open(path);
+      const stats = await fd.stat();
+      expect(stats.isFile()).toBeTrue();
+      expect(stats.size).toBeGreaterThan(0);
+
+      await fd.close();
+    });
+
+    afterAll(async () => {
+      await fs.promises.unlink(path).catch((err) => {
+        if (err.code !== "ENOENT")
+          throw new Error("Unexpected error", { cause: err });
+      });
+    });
+  });
+
+  describe("match()", async () => {
+    const req = new Request("https://www.example.com/match");
+    const body = { foo: "bar" };
+    const res = Response.json(body);
+    let path: PathLike;
+
+    beforeAll(async () => {
+      await cache.put(req, res);
+      path = cache["path"](await cache["hashRequestInfo"](req.url));
+    });
+
+    it("can return a Response for the given RequestInfo", async () => {
+      const promise = cache.match(req);
+      expect(promise).resolves.toBeInstanceOf(Response);
+
+      const alsoRes = await promise;
+
+      if (alsoRes) {
+        expect(alsoRes.json()).resolves.toEqual(body);
+        expect(alsoRes.status).toBe(res.status);
+        expect(alsoRes.statusText).toBe(res.statusText);
+        expect(alsoRes.headers.toJSON()).toEqual(res.headers.toJSON());
+      }
+    });
+
+    it("will return undefined for a URL not in the cache", async () => {
+      const promise = cache.match("https://someuncachedwebsite.com");
+      expect(promise).resolves.toBeUndefined();
+    });
+
+    afterAll(async () => {
+      await fs.promises.unlink(path).catch((err) => {
+        if (err.code !== "ENOENT")
+          throw new Error("Unexpected error", { cause: err });
+      });
+    });
+  });
+
+  describe("add()", async () => {
+    const req = new Request("https://www.example.com/add");
+    const path = cache["path"](await cache["hashRequestInfo"](req));
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it("can fetch the Response for the a given URL and save it", async () => {
+      const fetchSpy = spyOn(globalThis, "fetch").mockResolvedValueOnce(Response.json({ foo: "bar" }));
+      const promise = cache.add(req);
+      expect(promise).resolves.toBeUndefined();
+      expect(fetchSpy).toHaveBeenCalled();
+
+      await promise;
+
+      const fd = await fs.promises.open(path);
+      const stats = await fd.stat();
+      expect(stats.isFile()).toBeTrue();
+      expect(stats.size).toBeGreaterThan(0);
+
+      await fd.close();
+    });
+
+    it("will error if Response is not okay", async () => {
+      const fetchSpy = spyOn(globalThis, "fetch").mockResolvedValueOnce(Response.error());
+      const promise = cache.add(req);
+      expect(promise).rejects.toBeInstanceOf(TypeError);
+      expect(fetchSpy).toHaveBeenCalled();
+    });
+
+    afterAll(async () => {
+      await fs.promises.unlink(path).catch((err) => {
+        if (err.code !== "ENOENT")
+          throw new Error("Unexpected error", { cause: err });
+      });
+    });
   });
 });
--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -16,12 +16,15 @@ class FileSystemCache implements Cache {
   private static classInitPromise: Promise<any>;
   private initPromise: Promise<any>;
   private dirname: URL;
-  private memoryCache: Map<RequestInfo | URL, Response>;
+  private hashes: Map<RequestInfo | URL, string>;
 
   static {
     logger("Running static initialization block");
     this.classInitPromise = fsp.mkdir(CACHES_DIR, { recursive: true })
-      .then(str => logger("Resolved with value %s", str));
+      .then((str) => {
+        if (str)
+          logger("Created `CACHES_DIR` at %s", str);
+      });
   }
 
   private constructor(name: string) {
@@ -30,7 +33,7 @@ class FileSystemCache implements Cache {
       throw new TypeError(`Use static method ${funcName}() instead to create an instance.`);
     }
     this.dirname = new URL(`${name}/`, CACHES_DIR);
-    this.memoryCache = new Map();
+    this.hashes = new Map();
     this.initPromise = fsp.mkdir(this.dirname, { recursive: true });
     FileSystemCache.internal = false;
   }
@@ -47,61 +50,103 @@ class FileSystemCache implements Cache {
     return promises.then(() => instance);
   }
 
-  private static hashResponse(response: Response) {
-    const str = response.headers.get("Date");
-    const date = str ? new Date(str) : new Date();
-    return date.toISOString().slice(0, 10);
+  private async hashRequestInfo(info: RequestInfo | URL): Promise<string> {
+    let hash = this.hashes.get(info);
+
+    if (hash)
+      return hash;
+
+    const encoder = new TextEncoder();
+    const isRequest = info instanceof Request;
+
+    let url: URL;
+    let method: string;
+    let headers: Headers;
+
+    if (isRequest) {
+      url = new URL(info.url);
+      method = info.method;
+      headers = info.headers;
+    }
+    else {
+      url = new URL(info);
+      method = "GET";
+      headers = new Headers();
+    }
+
+    // Create an alphabetically sorted array header entries
+    const sortedHeaders = Array.from(headers.entries())
+      .sort((a, b) => a[0].at(0)!.localeCompare(b[0].at(0)!));
+
+    const dataString = method
+      + url.toString()
+      + JSON.stringify(sortedHeaders);
+
+    hash = await crypto.subtle.digest("SHA-1", encoder.encode(dataString))
+      .then(buf => Array.from(new Uint8Array(buf)))
+      .then(arr => arr.map(b => b.toString(16).padStart(2, "0")).join("")) satisfies string;
+
+    this.hashes.set(info, hash);
+    return hash;
+  }
+
+  private path(hash: string) {
+    return new URL(hash, this.dirname);
+  }
+
+  private async serializeResponse(response: Response): Promise<Uint8Array> {
+    const init = {
+      headers: response.headers.toJSON(),
+      status: response.status,
+      statusText: response.statusText,
+    } satisfies ResponseInit;
+
+    const initBuffer = new TextEncoder().encode(JSON.stringify(init));
+    const bodyBuffer = await response.arrayBuffer().then(ab => new Uint8Array(ab));
+
+    return Uint8Array.of(
+      ...initBuffer,
+      0, // Null-char
+      ...bodyBuffer,
+    );
+  }
+
+  private async deserializeResponse(buffer: Uint8Array): Promise<Response> {
+    const i = buffer.indexOf(0); // Null-char
+    const init = JSON.parse(new TextDecoder().decode(buffer.slice(0, i))) as ResponseInit;
+    const body = buffer.slice(i + 1);
+
+    return new Response(body, init);
+  }
+
+  private async write(info: RequestInfo | URL, response: Response) {
+    const hash = this.hashes.get(info) ?? await this.hashRequestInfo(info);
+    return this.serializeResponse(response)
+      .then(buffer => fsp.writeFile(this.path(hash), buffer));
+  }
+
+  private async read(info: RequestInfo | URL) {
+    const hash = this.hashes.get(info) ?? await this.hashRequestInfo(info);
+    return fsp.readFile(this.path(hash))
+      .then(buffer => this.deserializeResponse(buffer));
   }
 
   async put(info: RequestInfo | URL, response: Response): Promise<void> {
-    this.memoryCache.set(info, response);
     const isRequest = info instanceof Request;
+    const urlString = isRequest ? info.url : info.toString();
 
-    const url = isRequest
-      ? new URL(info.url)
-      : new URL(info);
+    if (!urlString.startsWith("http"))
+      throw new TypeError("URL scheme is not `http` or `https`");
 
-    const request = !isRequest
-      ? new Request(url)
-      : info.clone();
-
-    const subdirname = new URL(`${url.hostname}/${url.pathname.slice(1)}/`, this.dirname);
-    const metadataPath = new URL("./metadata.json", subdirname);
-    const responsePath = new URL(`./body-${FileSystemCache.hashResponse(response)}.blob`, subdirname);
-
-    const metadata = {
-      meta: {
-        url,
-        method: request.method,
-      },
-      request: {
-        headers: request.headers.toJSON(),
-        body: request.body && await request.text(),
-      },
-      response: {
-        headers: response.headers.toJSON(),
-        body: response.body && responsePath,
-      },
-    };
-
-    const promise = fsp.mkdir(subdirname, { recursive: true }).then(() =>
-      Promise.all([
-        fsp.writeFile(metadataPath, JSON.stringify(metadata, null, 2)),
-        response.body && response.arrayBuffer()
-          .then(buf => new Uint8Array(buf))
-          .then(view => fsp.writeFile(responsePath, view)),
-      ]),
-    );
-
-    return promise
-      .then(() => logger("Saved response to cache!"))
-      .catch((err) => {
-        throw new Error("Unexpected error occurred when saving files", { cause: err });
-      });
+    return this.write(info, response);
   }
 
-  add(request: RequestInfo | URL): Promise<void> {
-    throw new Error("Method not implemented.");
+  async add(info: RequestInfo | URL): Promise<void> {
+    return fetch(info).then((res) => {
+      if (!res.ok)
+        throw new TypeError("Bad response status", { cause: res });
+      return this.put(info, res);
+    });
   }
 
   addAll(requests: RequestInfo[]): Promise<void>;
@@ -118,8 +163,19 @@ class FileSystemCache implements Cache {
     throw new Error("Method not implemented.");
   }
 
-  match(request: RequestInfo | URL, options?: CacheQueryOptions | undefined): Promise<Response | undefined> {
-    throw new Error("Method not implemented.");
+  async match(info: RequestInfo | URL, options?: CacheQueryOptions | undefined): Promise<Response | undefined> {
+    if (options)
+      throw new Error("CacheQueryOptions not implemented.");
+
+    return this.read(info)
+      .catch((err) => {
+        if (err.code === "ENOENT")
+          return undefined;
+        throw new Error(
+          "Unexpected error when calling `cache.match()`",
+          { cause: err },
+        );
+      });
   }
 
   matchAll(request?: RequestInfo | URL | undefined, options?: CacheQueryOptions | undefined): Promise<readonly Response[]> {
