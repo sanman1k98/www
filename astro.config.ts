3283fa3f1751d9424bedd0a34b523f0925f5545f
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-06-23 14:56:06 -0400

    refactor: use experimental `astro:env`
    - change from "BUILD_INCLUDE_PERSONAL" to "BUILD='personal'"
    - remove `utils.env`

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,31 @@
-import { defineConfig, sharpImageService } from "astro/config";
+import { defineConfig, envField, sharpImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import UnoCSS from "unocss/astro";
 
+const envSchema = {
+  BUILD: envField.enum({
+    values: ["personal"],
+    context: "server",
+    access: "public",
+    optional: true,
+  }),
+  PERSONAL_EMAIL: envField.string({
+    context: "server",
+    access: "secret",
+    default: "placeholder@example.com",
+  }),
+  PERSONAL_PHONE: envField.string({
+    context: "server",
+    access: "secret",
+    default: "+1-800-NOT-REAL",
+  }),
+};
+
 export default defineConfig({
   site: "https://nicesandeep.com",
   prefetch: { prefetchAll: true },
+  image: { service: sharpImageService() },
   integrations: [
     sitemap(),
     icon(),
@@ -14,7 +34,9 @@ export default defineConfig({
       injectReset: true,
     }),
   ],
-  image: { service: sharpImageService() },
+  experimental: {
+    env: { schema: envSchema },
+  },
   vite: {
     optimizeDeps: { exclude: ["@resvg/resvg-js"] },
   },
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,4 @@ import type { theme as miniTheme } from "@unocss/preset-mini";
 import { resolveConfig } from "@unocss/core";
 import unoConfig from "uno.config";
 
-export { env } from "./env";
-
 export const { theme: unoTheme } = resolveConfig<typeof miniTheme>(unoConfig);
