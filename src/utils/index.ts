21c89ee467b63b23a0094ef1b7ef85f8ac1cc9da
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-06-30 13:01:56 -0400

    refactor(utils): import from default `unocss`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
-import type { theme as miniTheme } from "@unocss/preset-mini";
-import { resolveConfig } from "@unocss/core";
 import unoConfig from "uno.config";
+import { resolveConfig } from "unocss";
+import type { theme as miniTheme } from "unocss/preset-mini";
 
 export const { theme: unoTheme } = resolveConfig<typeof miniTheme>(unoConfig);
