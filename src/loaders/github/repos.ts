bba9ce923ae7cdea4ea6767f6afaa2c9647eb8f8
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-08-17 23:50:22 -0400

    refactor(loader): implement basic caching logic

--- src/loaders/github/repos.ts
+++ src/loaders/github/repos.ts
@@ -1,4 +1,5 @@
-import { styleText as style } from "node:util";
+import { inspect, styleText as style } from "node:util";
+import { z } from "astro/zod";
 import type { Loader } from "astro/loaders";
 import type { paths } from "@octokit/openapi-types";
 
@@ -8,6 +9,7 @@ export interface GitHubReposLoaderConfig {
   sort?: "created" | "updated" | "pushed" | "full_name";
   direction?: "asc" | "desc";
   results?: number;
+  /** How long to cache the responses. */
   maxAge?: number;
   user: string;
 };
@@ -15,37 +17,62 @@ export interface GitHubReposLoaderConfig {
 export function githubReposLoader({
   user,
   results = 30,
+  maxAge = 3600,
   sort = "pushed",
   direction = "desc",
 }: GitHubReposLoaderConfig): Loader {
-  const headers = { "Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28" };
-  const endpoint = `https://api.github.com/users/${user}/repos`;
-  const params = new URLSearchParams({ sort, direction, per_page: results.toString() });
-  const url = new URL(`${endpoint}?${params}`);
-  const req = new Request(url, { headers });
+  const fetchRepos = async (etag?: string) => {
+    const params = new URLSearchParams({
+      per_page: results.toString(),
+      direction,
+      sort,
+    });
+
+    const headers: HeadersInit = [
+      ["Accept", "application/vnd.github+json"],
+      ["X-GitHub-Api-Version", "2022-11-28"],
+    ];
+
+    if (etag)
+      headers.push(["If-None-Match", etag]);
+
+    return fetch(
+      `https://api.github.com/users/${user}/repos?${params}`,
+      { headers },
+    );
+  };
 
   return {
     name: "github-repos-loader",
+    schema: z.object({}).passthrough(),
     load: async ({ logger, meta, store }) => {
-      logger.info(`Loading GitHub repos for ${style("bold", user)}`);
+      logger.info(`Fetching from GitHub`);
+      const res = await fetchRepos(meta.get("etag"));
 
-      const _lastModified = meta.get("last-modified");
-      const _etag = meta.get("etag");
-
-      const res = await fetch(req);
+      if (res.status === 304)
+        return logger.info("Using cached response");
 
       if (!res.ok || !res.body) {
         throw new Error(`Failed to load GitHub repos for user: ${user}`);
       }
 
-      const repos = await res.json() as GitHubRepos;
+      const etag = res.headers.get("Etag");
+      const date = res.headers.get("Date");
 
-      for (const { full_name, ...repo } of repos) {
-        logger.info(`Loading ${style("bold", full_name)} repo info.`);
-        store.set({ id: full_name, data: repo });
+      if (etag === null || date === null) {
+        logger.error(inspect(res));
+        throw new Error(`Missing headers in response`);
+      } else {
+        meta.set("etag", etag);
+        meta.set("date", date);
       }
 
-      logger.info(`Finished loading repos for ${style("bold", user)}.`);
+      const repos = await res.json() as GitHubRepos;
+
+      for (const repo of repos) {
+        logger.info(`Loading ${style("bold", repo.full_name)} repo info.`);
+        store.set({ id: repo.full_name, data: repo });
+      }
     },
   };
 }
