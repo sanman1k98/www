9e0a29cf3a511acfa3dc529bc701c401b45015aa
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-08-20 13:46:48 -0400

    refactor(loader): restructure repos loader
    - define default opts
    - restructure response handling
    - don't get dates from response headers
    - move log points
    - style logging output
    - add comments

--- src/loaders/github/repos.ts
+++ src/loaders/github/repos.ts
@@ -1,26 +1,33 @@
-import { inspect, styleText as style } from "node:util";
+/**
+ * @file A content loader to get data about a user's repositories. Intended to be referenced by
+ * entries in the "cv" collection.
+ */
+import { styleText as style } from "node:util";
 import { z } from "astro/zod";
 import type { Loader } from "astro/loaders";
 import type { paths } from "@octokit/openapi-types";
 
-export type GitHubRepos = paths["/users/{username}/repos"]["get"]["responses"]["200"]["content"]["application/json"];
+export type GitHubRepo = paths["/users/{username}/repos"]["get"]["responses"]["200"]["content"]["application/json"][number];
 
-export interface GitHubReposLoaderConfig {
+export interface GitHubReposLoaderOptions {
   sort?: "created" | "updated" | "pushed" | "full_name";
   direction?: "asc" | "desc";
   results?: number;
-  /** How long to cache the responses. */
+  /** Number of seconds to store response data. */
   maxAge?: number;
   user: string;
 };
 
-export function githubReposLoader({
-  user,
-  results = 30,
-  maxAge = 3600,
-  sort = "pushed",
-  direction = "desc",
-}: GitHubReposLoaderConfig): Loader {
+const DEFAULT_OPTS = {
+  results: 30,
+  maxAge: 3600,
+  sort: "pushed",
+  direction: "desc",
+} satisfies Omit<GitHubReposLoaderOptions, "user">;
+
+export function githubReposLoader(opts: GitHubReposLoaderOptions): Loader {
+  const { user, results, maxAge, sort, direction } = { ...DEFAULT_OPTS, ...opts };
+
   const fetchRepos = async (etag?: string) => {
     const params = new URLSearchParams({
       per_page: results.toString(),
@@ -46,39 +53,56 @@ export function githubReposLoader({
     name: "github-repos-loader",
     schema: z.object({}).passthrough(),
     load: async ({ logger, meta, store }) => {
-      if (meta.has("date") && Date.now() - new Date(meta.get("date")!).valueOf() < maxAge * 1000)
-        return logger.info("Skipping fetch, using cached data");
+      const username = style(["bold", "blue"], user);
+      const { length: numEntries } = store.keys();
 
-      logger.info(`Fetching from GitHub`);
+      logger.info(`Loading public repositories for ${username}`);
+
+      // In order to avoid making network requests and API rate-limiting, we assume the list of
+      // GitHub repos for a given user does not change frequently and therefore data within the
+      // store if determined to be "fresh" will almost always be up-to-date and accurate.
+      if (meta.has("date")) {
+        const lastDate = new Date(meta.get("date")!);
+        const storeAgeMs = Date.now() - lastDate.valueOf();
+        const fresh = storeAgeMs < maxAge * 1000;
+
+        const format: Intl.DateTimeFormatOptions = { dateStyle: "short", timeStyle: "medium" };
+        const formattedLastDate = style(
+          fresh ? ["green"] : ["dim", "gray"],
+          lastDate.toLocaleString(undefined, format),
+        );
+        logger.info(`Last requested: ${formattedLastDate}`);
+
+        if (fresh)
+          return logger.info(`Loaded ${numEntries} entries ${style(["dim", "gray"], "(skipping fetch)")}`);
+      }
+
+      logger.info(`Fetching list of repositories for ${username}`);
       const res = await fetchRepos(meta.get("etag"));
+      const date = Date();
 
-      logger.debug(inspect(res));
-
-      if (res.status === 304)
-        return logger.info("Unmodified resposnse, Using cached data");
-
-      if (!res.ok || !res.body) {
-        logger.error(inspect(res));
-        throw new Error(`Failed to load GitHub repos for user: ${user}`);
-      }
-
-      const etag = res.headers.get("Etag");
-      const date = res.headers.get("Date");
-
-      if (etag === null || date === null) {
-        logger.error(inspect(res));
-        throw new Error(`Missing headers in response`);
-      } else {
-        meta.set("etag", etag);
+      if (res.status === 304) {
         meta.set("date", date);
+        return logger.info(`${numEntries} entries are up-to-date with GitHub`);
       }
 
-      const repos = await res.json() as GitHubRepos;
-      logger.info(`Loading info for ${repos.length} repos`);
+      if (res.status === 200 && res.body && res.headers.has("etag")) {
+        store.clear();
+        const repos = await res.json() as GitHubRepo[];
 
-      for (const repo of repos) {
-        store.set({ id: repo.full_name, data: repo });
+        // TODO: use the parseData() function from the LoaderContext.
+        for (const repo of repos) {
+          store.set({ id: repo.full_name, data: repo });
+        }
+
+        meta.set("etag", res.headers.get("etag")!);
+        meta.set("date", date);
+
+        return logger.info(`Loaded ${repos.length} entries`);
       }
+
+      // If we got here then we messed up something.
+      logger.error("Missing conditional logic for data fetching from GitHub!");
     },
   };
 }
