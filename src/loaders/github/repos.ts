32ff987273e8d0202b8a0d266646a1b19acf1ad3
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-08-22 13:30:24 -0400

    refactor(loaders): another big restructuring
    Add comments, move stuff around, and try to make things more readable (I
    hope).

--- src/loaders/github/repos.ts
+++ src/loaders/github/repos.ts
@@ -2,19 +2,19 @@
  * @file A content loader to get data about a user's repositories. Intended to be referenced by
  * entries in the "cv" collection.
  */
-import { styleText as style } from "node:util";
 import { z } from "astro/zod";
 import type { Loader } from "astro/loaders";
-import type { paths } from "@octokit/openapi-types";
+import type { operations, paths } from "@octokit/openapi-types";
+import { styles as c, format as fmt } from "@/utils/logging";
 
 export type GitHubRepo = paths["/users/{username}/repos"]["get"]["responses"]["200"]["content"]["application/json"][number];
 
-export interface GitHubReposLoaderOptions {
-  sort?: "created" | "updated" | "pushed" | "full_name";
-  direction?: "asc" | "desc";
-  results?: number;
+type QueryParams = NonNullable<operations["repos/list-for-org"]["parameters"]["query"]>;
+
+export interface GitHubReposLoaderOptions extends Omit<QueryParams, "page" | "per_page"> {
   /** Number of seconds to store response data. */
   maxAge?: number;
+  results?: number;
   user: string;
 };
 
@@ -28,6 +28,13 @@ const DEFAULT_OPTS = {
 export function githubReposLoader(opts: GitHubReposLoaderOptions): Loader {
   const { user, results, maxAge, sort, direction } = { ...DEFAULT_OPTS, ...opts };
 
+  /**
+   * Make a request to get a list of repositories for the {@link user}.
+   * @param etag - An `Etag` header value from a previous response from this endpoint.
+   * @see https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Conditional_requests
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#validation
+   */
   const fetchRepos = async (etag?: string) => {
     const params = new URLSearchParams({
       per_page: results.toString(),
@@ -52,57 +59,66 @@ export function githubReposLoader(opts: GitHubReposLoaderOptions): Loader {
   return {
     name: "github-repos-loader",
     schema: z.object({}).passthrough(),
-    load: async ({ logger, meta, store }) => {
-      const username = style(["bold", "blue"], user);
-      const { length: numEntries } = store.keys();
 
-      logger.info(`Loading public repositories for ${username}`);
+    // In order to avoid making network requests and API rate-limiting, we assume the list of
+    // GitHub repos for any given user does not change frequently and therefore entries within a
+    // fresh store will almost always be up-to-date and accurate.
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#fresh_and_stale_based_on_age
+    load: async ({ logger, meta, store, collection }) => {
+      const done = (reason: string) =>
+        logger.info(fmt(
+          `Loaded ${c.I`%d`} entries in ${c.green`%s`} ${c.muted`(%s)`}`,
+          store.keys().length,
+          collection,
+          reason,
+        ));
 
-      // In order to avoid making network requests and API rate-limiting, we assume the list of
-      // GitHub repos for a given user does not change frequently and therefore data within the
-      // store if determined to be "fresh" will almost always be up-to-date and accurate.
-      if (meta.has("date")) {
-        const lastDate = new Date(meta.get("date")!);
-        const storeAgeMs = Date.now() - lastDate.valueOf();
-        const fresh = storeAgeMs < maxAge * 1000;
+      logger.info(`Loading public repositories for ${c.blue(user)}`);
+      let fresh: boolean | undefined;
+
+      // Determine the state of the store and return early if it is fresh.
+      if (meta.has("last-modified")) {
+        const lastModDate = new Date(meta.get("last-modified")!);
+        const storeAgeMs = Date.now() - lastModDate.valueOf();
+        fresh = storeAgeMs < maxAge * 1000;
 
         const format: Intl.DateTimeFormatOptions = { dateStyle: "short", timeStyle: "medium" };
-        const formattedLastDate = style(
-          fresh ? ["green"] : ["dim", "gray"],
-          lastDate.toLocaleString(undefined, format),
-        );
-        logger.info(`Last requested: ${formattedLastDate}`);
+        const datestring = lastModDate.toLocaleString(undefined, format);
+        logger.info(fmt(`Last updated: ${c.U`%s`} ${c.muted`(%s)`}`, datestring, fresh ? "fresh" : "stale"));
 
         if (fresh)
-          return logger.info(`Loaded ${numEntries} entries ${style(["dim", "gray"], "(skipping fetch)")}`);
+          return done("used cache");
       }
 
-      logger.info(`Fetching list of repositories for ${username}`);
+      logger.info(`Fetching list of repositories for ${c.blue(user)}`);
       const res = await fetchRepos(meta.get("etag"));
       const date = Date();
 
-      if (res.status === 304) {
-        meta.set("date", date);
-        return logger.info(`${numEntries} entries are up-to-date with GitHub`);
+      // If the response has not changed, you will receive a 304 Not Modified response. Making a
+      // conditional request does not count against your primary rate limit if 304 is returned.
+      // https://docs.github.com/en/rest/using-the-rest-api/best-practices-for-using-the-rest-api?apiVersion=2022-11-28#use-conditional-requests-if-appropriate
+      if (fresh === false && res.status === 304) {
+        meta.set("last-modified", date);
+        return done("revalidated cache");
       }
 
       if (res.status === 200 && res.body && res.headers.has("etag")) {
         store.clear();
         const repos = await res.json() as GitHubRepo[];
 
-        // TODO: use the parseData() function from the LoaderContext.
+        // Use the repository's full name as keys e.g., "sanman1k98/www".
         for (const repo of repos) {
+          // TODO: use the parseData() function from the LoaderContext.
           store.set({ id: repo.full_name, data: repo });
         }
 
         meta.set("etag", res.headers.get("etag")!);
-        meta.set("date", date);
-
-        return logger.info(`Loaded ${repos.length} entries`);
+        meta.set("last-modified", date);
+        return done("cached response");
       }
 
       // If we got here then we messed up something.
-      logger.error("Missing conditional logic for data fetching from GitHub!");
+      logger.error("Did not load repositories!");
     },
   };
 }
