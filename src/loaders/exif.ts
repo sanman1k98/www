95d7ff57dc1824234d2a894867e47ef030ec9426
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-08-23 22:08:25 -0400

    feat(loaders): implement EXIF loader

--- src/loaders/exif.ts
+++ src/loaders/exif.ts
@@ -1,13 +1,37 @@
+/* eslint-disable ts/no-restricted-types */
+/* eslint-disable no-restricted-globals */
 /**
  * @file A content loader to get get EXIF metadata from local images.
  */
 import { glob } from "node:fs/promises";
-import { basename, relative } from "node:path/posix";
+import { relative } from "node:path/posix";
 import { fileURLToPath } from "node:url";
 import type { Loader } from "astro/loaders";
+import type { Exif, GenericTag } from "exif-reader";
+import exifReader from "exif-reader";
+import sharp from "sharp";
+import { z } from "astro/zod";
 import { styles as c } from "@/utils/logging";
 
-interface EXIFLoaderOptions {
+// see: https://stackoverflow.com/questions/1821515/how-is-exif-info-encoded#14115795
+type IFD = keyof Omit<Exif, "bigEndian">;
+type ExifTags = { [key in IFD]?: Exif[key] };
+
+/** Mutates the given object by converting any Buffers into number arrays. */
+function fixExif(data: ExifTags) {
+  for (const group of Object.entries(data)) {
+    const [_, tags] = group as [IFD, NonNullable<ExifTags[IFD]>];
+    for (const tag of Object.entries(tags)) {
+      const [name, value] = tag;
+      tags[name] = value instanceof Uint8Array
+        ? Array.from(value)
+        : value as Exclude<GenericTag, Buffer>;
+    }
+  }
+  return data;
+};
+
+interface ExifLoaderOptions {
   /** The glob pattern to match files, relative to the base directory */
   pattern: string;
   /**
@@ -17,7 +41,7 @@ interface EXIFLoaderOptions {
   base?: string | URL;
 };
 
-export function exifLoader(opts: EXIFLoaderOptions): Loader {
+export function exifLoader(opts: ExifLoaderOptions): Loader {
   if (opts.pattern.startsWith("../")) {
     throw new Error(
       "Glob patterns cannot start with `../`. Set the `base` option to a parent directory instead.",
@@ -30,7 +54,32 @@ export function exifLoader(opts: EXIFLoaderOptions): Loader {
   }
   return {
     name: "exif-loader",
-    load: async ({ logger, settings }) => {
+    schema: () => {
+      // type ExcludeBufferValues<T extends Record<string, any>> = {
+      //   [k in keyof T]: Exclude<T[k], Buffer>
+      // };
+      // type PojoExifTags = {
+      //   [k in keyof ExifTags]: ExcludeBufferValues<NonNullable<ExifTags[k]>> | undefined
+      // };
+
+      const GenericTagSchema = z.union([
+        z.string(),
+        z.number(),
+        z.number().array(),
+        z.date(),
+      ]).optional() satisfies z.ZodType<Exclude<GenericTag, Buffer> | Date | undefined>;
+
+      const ExifTagsSchema = z.strictObject({
+        Image: z.record(GenericTagSchema),
+        Photo: z.record(GenericTagSchema),
+        GPSInfo: z.record(GenericTagSchema),
+        Iop: z.record(GenericTagSchema),
+        ThumbnailTags: z.record(GenericTagSchema),
+      }).partial({ GPSInfo: true, Iop: true, ThumbnailTags: true });
+
+      return ExifTagsSchema;
+    },
+    load: async ({ logger, settings, parseData, store, generateDigest }) => {
       const baseDir = opts.base
         ? new URL(opts.base, settings.config.root)
         : settings.config.root;
@@ -40,13 +89,40 @@ export function exifLoader(opts: EXIFLoaderOptions): Loader {
       if (!baseDir.pathname.endsWith("/"))
         baseDir.pathname += "/";
 
-      const files = glob(opts.pattern, { cwd: fileURLToPath(baseDir) });
+      const files = await Array.fromAsync(glob(opts.pattern, { cwd: fileURLToPath(baseDir) }));
 
-      for await (const file of files) {
-        const id = getId(file);
-        const name = basename(id);
-        logger.info(`Found file ${c.green(id)} with name ${c.blue(name)}`);
+      const results = await Promise.all(
+        files.map(
+          async (filePath) => {
+            const id = getId(filePath);
+            // logger.info(`Parsing EXIF for ${id}`);
+            const { exif: buffer } = await sharp(filePath).metadata();
+            if (!buffer) {
+              const error = new Error(`Image ${id} does not contain any EXIF metadata.`);
+              return { id, error };
+            }
+            const { bigEndian: _, ...exifTags } = exifReader(buffer);
+            return await parseData({ id, data: fixExif(exifTags) })
+              .then((data) => {
+                store.set({ id, data, filePath, digest: generateDigest(data) });
+                return { id, error: null };
+              })
+              .catch(error => ({ id, error }));
+          },
+        ),
+      );
+
+      logger.info(`Found ${c.B(results.length)} images matching ${c.blue(opts.pattern)}`);
+      let loaded = results.length;
+
+      for (const { id, error } of results) {
+        if (error) {
+          logger.error(`Did not load data for ${c.green(id)}\n${c.red}`);
+          loaded--;
+        }
       }
+
+      logger.info(`Loaded ${c.B(loaded)} entries`);
     },
   };
 }
