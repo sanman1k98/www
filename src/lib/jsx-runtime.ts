db2de5025e4c5d5b2bf64dbf7d4856741292c1b5
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-12 14:26:56 -0400

    refactor(lib): custom JSX type definitions instead of aliasing React's
    Redefine JSXNode and JSXElement types to be mostly subtypes of their
    React counterparts.
    
    Make exported JSX runtime functions return the same type as the React
    JSX runtime functions.
    
    Add a bunch of docstrings and TODOs.

--- src/lib/jsx-runtime.ts
+++ src/lib/jsx-runtime.ts
@@ -1,40 +1,91 @@
-import type { ReactElement, JSX as ReactJSX, ReactNode } from 'react';
+import type { JSX as ReactJSX } from 'react';
 
+// TODO: define custom subset of style properties supported by Satori.
 export type { CSSProperties as JSXStyleProperties } from 'react';
 
-export type JSXNode = ReactNode;
-export type JSXElement<P = Record<string, any>> = ReactElement<P>;
+export interface JSXElement<
+	P = unknown,
+	T extends string | FC<P> = string | FC<P>,
+> {
+	type: T;
+	props: P;
+	key: string | null;
+};
+
+export type JSXNode =
+	| JSXElement
+	| string
+	| number
+	| bigint
+	| Iterable<JSXNode>
+	| boolean
+	| null
+	| undefined;
+
+// TODO: define supported Satori props.
+export interface SatoriJSXProps {
+	/**
+	 * Tailwind classes powered by `twrnc`.
+	 *
+	 * @see {@link https://github.com/jaredh159/tailwind-react-native-classnames `twrnc` documentation}
+	 * @experimental
+	 */
+	tw?: string;
+	[propName: string]: any;
+}
+
 export type FC<P = Record<string, any>> = (props: P) => JSXElement<P>;
 
 // eslint-disable-next-line ts/no-namespace
 export namespace JSX {
-	export type ElementType = string | FC;
-	export type Element = JSXElement;
-
-	// Don't support Class components.
+	/**
+	 * **WARNING**: Satori does not support class components, only pure and stateless functional components.
+	 *
+	 * @see {@link https://github.com/vercel/satori?tab=readme-ov-file#jsx Satori JSX documentation}
+	 */
 	export type ElementClass = never;
+	export type ElementType = string | FC;
+
+	export interface Element extends JSXElement<any, any> { };
 
 	export interface ElementChildrenAttribute {
 		children: JSXNode;
 	};
 
-	export type IntrinsicElements = ReactJSX.IntrinsicElements;
+	// TODO: define custom subset of IntrinsicElements supported by Satori.
+	export interface IntrinsicElements extends ReactJSX.IntrinsicElements { };
 
 	export interface IntrinsicAttributes {
-		key?: never;
+		/**
+		 * **INFO**: Allowed as prop, but will be ignored by Satori.
+		 */
+		key?: string | number | bigint | undefined | null;
+		/**
+		 * **WARNING**: React's `dangerouslySetInnerHTML` property is not supported by Satori.
+		 *
+		 * @see {@link https://github.com/vercel/satori?tab=readme-ov-file#jsx Satori JSX documentation}
+		 */
 		dangerouslySetInnerHTML?: never;
 	};
 }
 
-export function jsx(type: string | FC, props: Record<string, any>): JSXElement {
+export function jsx(
+	type: string | FC,
+	props: Record<string, any>,
+	key: string | null = null,
+): ReactJSX.Element {
 	if (typeof type === 'string') {
-		return { type, props, key: null };
+		return { type, props, key };
 	} else {
 		return type(props);
 	}
 }
 
-export function createElement(type: string | FC, props: Record<string, any> = {}, ...children: JSXNode[]): JSXElement {
+export function createElement(
+	type: string | FC,
+	props: Record<string, any> = {},
+	...children: JSXNode[]
+): ReactJSX.Element {
 	if (arguments.length > 3)
 		props.children = children;
 	return jsx(type, props);
