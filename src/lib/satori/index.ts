fca8f261e470e4d5fcefffa11905d709a6046050
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2025-08-22 11:54:18 -0400

    refactor: `createElement()`
    More in-line with React v19.1.
    
    Mirrors changes in my fork of Satori.

--- src/lib/satori/index.ts
+++ src/lib/satori/index.ts
@@ -1,14 +1,35 @@
-import type { FC, JSXNode } from './types.ts';
+import type { FC, JSXElement, JSXKey, JSXNode } from './types.ts';
 import { jsx } from './jsx-runtime.ts';
 
 export { Fragment, type JSX } from './jsx-runtime.ts';
 export type * from './types.ts';
 
-export function createElement(
-	type: string | FC,
-	props: Record<string, any> = {},
+/**
+ * Create a `ReactElement`-like object.
+ *
+ * @param type - Tag name string or a function component.
+ * @param props - Optional props to create the element with.
+ * @param children - Zero or more child nodes.
+ * @returns A `ReactElement`-like object with properties like `type`, `key`, `props`, and `props.children`.
+ */
+// eslint-disable-next-line ts/no-empty-object-type
+export function createElement<P extends {}>(
+	type: string | FC<P>,
+	props?: { key?: JSXKey | undefined | null } & Omit<P, 'children'>,
 	...children: JSXNode[]
-): JSXNode {
-	props.children = children;
-	return jsx(type, props);
+): JSXElement<P> {
+	if (!props) {
+		const newProps = children.length ? { children } : {};
+		return jsx(type, newProps, null) as JSXElement<P>;
+	}
+
+	// Destructure key from props.
+	const { key: keyProp = null, ...restProps } = props as Record<string, unknown>;
+	// Coerce key to string primitive.
+	const key = keyProp !== null ? String(keyProp) : null;
+
+	if (children.length)
+		restProps.children = children;
+
+	return jsx(type, restProps, key) as JSXElement<P>;
 }
